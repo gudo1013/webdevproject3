06610022f47fd8d3c53ded911edd7d9d4127f3f4
gudo1013 <gudo1013@stthomas.edu>
2021-11-29 14:42:06 -0600

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-let express = require('express') //npm install express
+let fs = require('fs');
 let path = require('path')
-let fs = require('fs')
+
+//node modules
+let sqlite3 = require('sqlite3');
+let express = require('express') //npm install express
 
 //user curl to test i believe
 
@@ -8,6 +11,15 @@ let port = 8000
 let public_dir = path.join(__dirname, 'public')
 let app = express()
 
+let db_filename = path.join(__dirname, 'stpaul_crime.sqlite3')
+
+//load in db
+let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+    if (err) {console.log('Error opening ' + db_filename); }
+    else {console.log('Now connected to ' + db_filename);}
+});
+
+app.use(express.static(public_dir))
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
@@ -15,9 +27,31 @@ app.get('/', (req, res) => {
 
 app.get('/codes', (req, res) => {
     console.log(req.query);
-    res.send('Hello world!' + req.query.hello);
+    //db.all('SELECT * from Consumption WHERE year = ? ORDER BY state_abbreviation', [req.params.selected_year], (err, row) => {
+    db.all('SELECT * from Codes ORDER BY code', (err, row) => {
+        res.send(row);
+    })
+    
 });
-app.use(express.static(public_dir))
+
+app.get('/neighborhoods', (req, res) => {
+    console.log(req.query);
+    //db.all('SELECT * from Consumption WHERE year = ? ORDER BY state_abbreviation', [req.params.selected_year], (err, row) => {
+    db.all('SELECT * from Neighborhoods ORDER BY neighborhood_number', (err, row) => {
+        res.send(row);
+    })
+    
+});
+
+app.get('/incidents', (req, res) => {
+    console.log(req.query);
+    //db.all('SELECT * from Consumption WHERE year = ? ORDER BY state_abbreviation', [req.params.selected_year], (err, row) => {
+    db.all('SELECT * from Incidents WHERE case_number = 21116050', (err, row) => {
+        res.send(row);
+    })
+    
+});
+
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
